1b36e5896b1d96a82c69376a472cedc7c2ae1f74
randydu <randyd@yummy.net>
2021-02-15 15:25:45 -0800

    tests: adds license-model-hard-date test;

--- license-data/sdk-test-0/license_data.h
+++ license-data/sdk-test-0/license_data.h
@@ -4,5 +4,6 @@
 extern const unsigned char sdk_test_0_lic_data_build_1[544];
 extern const unsigned char sdk_test_0_lic_data_build_2[541];
 extern const unsigned char sdk_test_0_lic_data_build_3[544];
+extern const unsigned char sdk_test_0_lic_data_build_4[699];
 
 #endif
---
+++ tests/sdk-test-0/main.h
@@ -0,0 +1,6 @@
+#ifndef SDK_TEST_0_MAIN_H_
+#define SDK_TEST_0_MAIN_H_
+
+void clean_license();
+
+#endif
